52e17bbdd8d5a3fdcb376b113d54ad58fbb2c0d2
admin <gorod3012@gmail.com>
2023-11-14 19:01:13 +0800

    * fix position CustomButton

--- src/ui/components/products/ProductInfoButton.tsx
+++ src/ui/components/products/ProductInfoButton.tsx
@@ -3,16 +3,20 @@ import {useAppDispatch, useAppSelector} from '../../../services/hooks';
 import {selectCurrentProduct} from '../../../services/store/reducers/products';
 import {increaseCounter} from '../../../services/store/reducers/cart';
 import CustomButton from '../common/CustomButton';
+import {View} from 'react-native';
 
 const ProductInfoButton = () => {
   const dispatch = useAppDispatch();
   const product = useAppSelector(selectCurrentProduct);
 
   return (
-    <CustomButton
-      onPress={() => dispatch(increaseCounter(product))}
-      title="Add to Cart"
-    />
+    <View style={{alignItems: 'flex-end'}}>
+      <CustomButton
+        onPress={() => dispatch(increaseCounter(product))}
+        title="Add to Cart"
+        style={{margin: 10}}
+      />
+    </View>
   );
 };
 
