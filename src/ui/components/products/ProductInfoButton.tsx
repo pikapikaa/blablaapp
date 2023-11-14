dc816ee66cab4b9ae1fe08cb051e4d12f40a4568
admin <gorod3012@gmail.com>
2023-11-14 21:13:46 +0800

    *fix detail screen

--- src/ui/components/products/ProductInfoButton.tsx
+++ src/ui/components/products/ProductInfoButton.tsx
@@ -3,14 +3,28 @@ import {useAppDispatch, useAppSelector} from '../../../services/hooks';
 import {selectCurrentProduct} from '../../../services/store/reducers/products';
 import {increaseCounter} from '../../../services/store/reducers/cart';
 import CustomButton from '../common/CustomButton';
-import {View} from 'react-native';
+import {Alert, Pressable, View} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const ProductInfoButton = () => {
   const dispatch = useAppDispatch();
   const product = useAppSelector(selectCurrentProduct);
 
   return (
-    <View style={{alignItems: 'flex-end'}}>
+    <View
+      style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      }}>
+      <Pressable
+        onPress={() => Alert.alert('Added to Wish List!')}
+        style={{alignItems: 'center', flex: 1}}>
+        <View>
+          <Icon name="heart-outline" size={30} color="black" />
+        </View>
+      </Pressable>
+
       <CustomButton
         onPress={() => dispatch(increaseCounter(product))}
         title="Add to Cart"
